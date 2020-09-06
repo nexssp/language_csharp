d1ab235f09eaa82668d6d5e68a0fcd00d5e72b2c
Marcin Polak <mapoart@gmail.com>
2020-09-06 19:48:32 +0200

    Fixed path to .bashtc

--- csharp.linux.nexss.config.js
+++ csharp.linux.nexss.config.js
@@ -1,8 +1,22 @@
 let languageConfig = Object.assign({}, require("./csharp.win32.nexss.config"));
 
+let sudo = "sudo ";
+if (process.getuid && process.getuid() === 0) {
+  sudo = "";
+}
+
 languageConfig.compilers = {
   dotnet: {
-    install: `snap install --classic dotnet-sdk && curl -s https://raw.githubusercontent.com/filipw/dotnet-script/master/install/install.sh | bash`,
+    install: `${sudo}wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+${sudo}dpkg -i packages-microsoft-prod.deb
+${sudo}add-apt-repository universe
+${sudo}apt-get install -y apt-transport-https curl
+${sudo}apt-get update
+${sudo}apt-get install -y dotnet-sdk-2.1
+curl -s https://raw.githubusercontent.com/filipw/dotnet-script/master/install/install.sh | bash
+${sudo}chmod +x ~/.dotnet/tools/dotnet-script
+grep -qxF 'export PATH="$HOME/.dotnet/tools/:$PATH"' ~/.bashrc || echo 'export PATH="$HOME/.dotnet/tools/:$PATH"' >> ~/.bashrc
+. ~/.bashrc`,
     command: `dotnet-script`,
     args: "<file>",
     help: ``,
